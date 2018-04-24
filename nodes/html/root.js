b256a1a1f04c50004e901c8a587a780596dbe148
forceuser <forceuserz@gmail.com>
2018-04-24 09:39:28 +0300

    wip

--- nodes/html/root.js
+++ nodes/html/root.js
@@ -1,32 +1,120 @@
 export default function (ctrl) {
-	while (!ctrl.isEnd()) {
-		const {start, node} = ctrl;
-		if (node.type === "root") {
-			if (ctrl.compare("<")) {
-				ctrl.move();
-				const tagName = ctrl.while(/[a-z]/i);
+	const parse = {
+		openTag () {
+			if (ctrl.match("<")) {
+				const tagName = ctrl.match(/^[a-z]+/i);
 				if (tagName) {
 					ctrl.start("tag", {tagName});
+					ctrl.go(tagName.length + 1);
+					return true;
 				}
-				else if (ctrl.compare("!--")) {
-					ctrl.start("comment");
+			}
+		},
+		openComment () {
+			if (ctrl.match("!--")) {
+				ctrl.start("comment");
+				ctrl.go(3);
+				return true;
+			}
+		},
+		closeComment () {
+			if (ctrl.match("-->")) {
+				ctrl.go(3).end();
+				return true;
+			}
+		},
+		closeTag () {
+			if (ctrl.match(">")) {
+				ctrl.go().end();
+				return true;
+			}
+		},
+		openAttr () {
+			const match = ctrl.match(/^(?<prefix>@[a-z]+)?:?(?<attr>[a-z-]+)?(?:=(?<quote>["']))?/i);
+			if (match) {
+				const {"0": full, prefix, attr, quote} = match.groups;
+				if (quote) {
+					ctrl.start("attr", {attr, prefix});
 				}
+				else {
+					ctrl.add("attr", {attr, prefix});
+				}
+				ctrl.go(full.length);
+				return true;
+			}
+		},
+		attr () {
+			const matchExpr = ctrl.match("{{");
+			if (matchExpr) {
+
+
+				ctrl.startCopy();
+				ctrl.go(matchExpr[0].length);
+				let m;
+				while (!m) {
+					m = ctrl.match(`}}${ctrl.quote}`);
+					!m && ctrl.go();
+				}
+				if (m) {
+					const expr = ctrl.endCopy();
+					ctrl.go(m[0].length).end();
+				}
+			}
+			else {
+				// parse string
+				const stringRegex = /^(("(?:[^"\\]|\\.)*")|('(?:[^'\\]|\\.)*'))/;
+				const match = ctrl.go(-1).match(stringRegex);
+				if (match) {
+					ctrl.node.value = match[0];
+					ctrl.go(match[0].length).end();
+				}
+			}
+		},
+	};
+
+	while (!ctrl.isEnd()) {
+		switch (ctrl.node.type) {
+			case "tag": {
+				[
+					parse.closeTag,
+					parse.openAttr,
+				].some(p => p());
+				break;
+			}
+			case "attr": {
+				parse.attr();
+				break;
+			}
+			default: {
+				parse.openTag();
+				break;
 			}
 		}
-		else if (node.type === "tag") {
-			if (ctrl.compare(">")) {
-				ctrl.end();
-			}
-			else if (ctrl.match(/[a-z]/i)) {
-				ctrl.start("attr");
-			}
-		}
-		else if (node.type === "attr") {
-			const attrName = ctrl.while(/[a-z-]/i);
-			if (ctrl.compare("=") {
-				ctrl.start("attr-value")
-			}
-		}_
+
 		ctrl.next();
 	}
 }
+
+
+// разбить на выражения игнорируя строки
+// определить правую и левую сторону выражения (const, let, var)
+// const a = b = c + 2;
+// находим начало литерала [a-z]
+// идем игнорируя строки пока [a-z]
+// x.y = a.b[c["x"]].d(attrs)
+
+
+// create empty expression, clear $ concat "x"
+// >. push "x"
+// clear $ concat "y"
+// >= push "y", mark expression as "set"
+// create empty expression, clear $ concat "a"
+// >. push "a"
+// clear $ concat b
+// >[ push "b",
+// >"c" create new expression
+
+
+
+// (get, set, call) => set(["x", "y"], call(get(["a", "b", get(["c", "x"]), "d"])), get(["attrs"]))
+// parse expression
--- parse-ctrl.js
+++ parse-ctrl.js
@@ -1,25 +1,10 @@
-function compare (str1, str2, idx1 = 0, idx2 = 0) {
-	let i = 0;
-	const l1 = str1.length;
-	const l2 = str2.length;
-	if (l1 < l2 - idx2 + idx1) {
-		return false;
-	}
-
-	while (i < l2) {
-		if (str1[idx1 + i] !== str2[idx2 + i]) {
-			return false;
-		}
-		i++;
-	}
-	return true;
-}
-
 export default class ParseCtrl {
 	constructor (text, idx = 0) {
 		this.text = text;
 		this.idx = idx;
-		this.stack = [{type: "root", children: []}];
+		this.root = {type: "root", children: []};
+		this.stack = [this.root];
+		this.copyStack = [];
 		Object.defineProperty(this.stack, "empty", {
 			get () {
 				return this.length === 0;
@@ -29,19 +14,35 @@ export default class ParseCtrl {
 			return this.stack[this.stack.length - 1];
 		};
 	}
-	add (node) {
+	add (nodeType, params) {
+		const node = Object.assign({}, params, {nodeType});
 		this.node.children.push(node);
 	}
-	get (length = 1, shift = 0) {
+	start (...args) {
+		const node = this.add(...args);
+		this.stack.push(node);
+		return node;
+	}
+	end () {
+		return this.stack.pop();
+	}
+	get (length, shift = 0) {
 		if (length === 1) {
 			return this.text.charAt(this.idx + shift);
 		}
 		return this.text.substr(this.idx + shift, length);
 	}
+	startCopy () {
+		this.copyStack.push(this.idx);
+	}
+	endCopy () {
+		const idx = this.copyStack.pop();
+		return this.text.substring(idx, this.idx);
+	}
 	get node () {
 		return this.stack[this.stack.length - 1];
 	}
-	move (shift = 1) {
+	go (shift = 1) {
 		this.idx += shift;
 		this.moved = true;
 		return this;
@@ -58,20 +59,32 @@ export default class ParseCtrl {
 	isEnd () {
 		return this.idx >= this.text.length;
 	}
-	compare (target, targetIdx = 0, srcIdx = this.idx) {
-		let i = 0;
-		const l1 = target.length;
-		const l2 = this.text.length;
-		if (l1 < l2 - srcIdx + targetIdx) {
+	match (val) {
+		const str = this.get();
+		if (val instanceof RegExp) {
+			return str.match(val);
+		}
+		return str.startsWith(val);
+	}
+}
+
+
+
+
+
+function compare (str1, str2, idx1 = 0, idx2 = 0) {
+	let i = 0;
+	const l1 = str1.length;
+	const l2 = str2.length;
+	if (l1 < l2 - idx2 + idx1) {
+		return false;
+	}
+
+	while (i < l2) {
+		if (str1[idx1 + i] !== str2[idx2 + i]) {
 			return false;
 		}
-
-		while (i < l2) {
-			if (target[targetIdx + i] !== this.text[srcIdx + i]) {
-				return false;
-			}
-			i++;
-		}
-		return true;
+		i++;
 	}
+	return true;
 }
