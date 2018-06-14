1ad999896f0732697a514bf06a71516caac86af9
forceuser <forceuserz@gmail.com>
2018-06-14 20:05:20 +0300

    wip

--- drafts/parse-expression-result.js
+++ drafts/parse-expression-result.js
@@ -1,10 +1,70 @@
-import { checkServerIdentity } from "tls";
-
-let a = 5;
-a = 5;
-
-return {
-	set a () {
+class View { // Basic class
+	constructor (options = {}) {
+		this.options = options;
+		this.model = options.model;
+		this.cache = new Cache();
+		this.init();
+		const domNode = options.domNode || document.createElement("div");
+		this.reactionCtrl = reaction(() => {
+			if (typeof options.onChange === "function") {
+				options.onChange();
+			}
+			this.vnode = patch(this.vnode || domNode, this.render({
+				model: this.model,
+				cache: this.cache,
+			}));
+		}, false);
+		this.reactionCtrl.reaction();
+	}
+	init () {
 
 	}
+	render () {
+		// abstract render
+	}
+	destroy () {
+		this.reactionCtrl.unregister();
+	}
 }
+
+class Cache () {
+	constructor () {
+		let storage = [];
+		let newStorage;
+		function cache (key, fn) {
+			let result;
+			if (key in storage) {
+				result = storage[key];
+				newStorage[key] = result;
+				return storage[key];
+			}
+			result = fn(new Cache());
+			newStorage[key] = result;
+			return result;
+		}
+		cache.begin = () => {
+			newStorage = [];
+		};
+		cache.end = () => {
+			storage = newStorage;
+		};
+
+		return cache;
+	}
+}
+
+
+
+new View({
+	model: {},
+	render ({cache}) {
+		return ["div", {}, [
+			...[1, 2].map(key => 
+				cache(key, cache => 
+					["span", {key}, [cache(0, )]]
+				)
+			)
+		]]
+	}
+
+})
